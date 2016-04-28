f6fe8f545d5958dcb0caa1422e5fa3db1ee3da7e
Secbone <secbone@gmail.com>
2016-04-28 12:21:44 +0800

    add test for custom store

--- test/test.js
+++ test/test.js
@@ -1,6 +1,30 @@
 import Koa from "koa";
 import request from "supertest";
-import session from "../dist/index.js";
+import session, {Store} from "../dist/index.js";
+
+
+class CustomStore extends Store {
+    constructor() {
+        super();
+        this.store = {};
+    }
+
+    async get(sid) {
+        return this.store[sid];
+    }
+
+    async set(session, opts) {
+        if(!opts.sid) {
+            opts.sid = this.getID(24);
+        }
+        this.store[opts.sid] = session;
+        return opts.sid;
+    }
+
+    async destory(sid) {
+        delete this.store[sid];
+    }
+}
 
 describe("koa-session2", () => {
     describe("when default", () => {
@@ -37,4 +61,25 @@ describe("koa-session2", () => {
             });
         });
     });
+
+    describe("when use custom store", () => {
+        it("should be work", done => {
+            let app = new Koa();
+            app.use(session({
+                store: new CustomStore()
+            }));
+
+            app.use(ctx => {
+                ctx.session.user = {
+                    name: "tom"
+                };
+
+                ctx.body = ctx.session;
+            });
+
+            request(app.listen())
+            .get("/")
+            .expect(200, done);
+        });
+    })
 })
